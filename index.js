0a5d6eefd2086f9462793af7d783974013433b2b
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-02 16:38:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,3 @@
 // such as "Welcome, Ada. You are number 1 in line.". And don't go being too programmer-y and give them their index.
 // These are normal people. If they are 7th in line, tell them that.
 // Don't get their hopes up by telling them they are number 6 in line.
-
-
