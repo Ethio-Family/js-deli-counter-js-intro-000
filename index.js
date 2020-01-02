7b65d2f2913efb1a9fcff4a72a35e346206b7731
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-02 16:40:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // Build a function that a new customer will use when entering the deli.
 // The function, takeANumber, should accept two parameters:
 // the current line of people, along with the new person's name.
@@ -5,3 +6,10 @@
 // such as "Welcome, Ada. You are number 1 in line.". And don't go being too programmer-y and give them their index.
 // These are normal people. If they are 7th in line, tell them that.
 // Don't get their hopes up by telling them they are number 6 in line.
+
+
+function takeANumber(line, name) {
+
+  line = line + 1;
+  return "Welcome, ${name}. You are number ${line} in line."
+}
